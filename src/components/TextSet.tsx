5ac3a8a1ab10da445bf4cd112812a43744d69aaf
yoelgarcialago <yoelgarcialago@gmail.com>
2026-01-25 21:05:41 +0100

    Componente input texto + label creado

---
+++ src/components/TextSet.tsx
@@ -0,0 +1,29 @@
+import type { Dispatch, SetStateAction } from "react"
+
+interface TextSetProps {
+    varText: string
+    setVarText: Dispatch<SetStateAction<string>>
+    
+    idText: string
+    labelText: string
+    placeholderText?: string
+}
+
+export default function TextSet(props: TextSetProps) {
+    return (
+        <div className="flex flex-col">
+            <label htmlFor={props.idText} className="text-lg font-medium text-gray-700">
+                {props.labelText}
+            </label>
+            <br />
+            <input
+                type="text"
+                id={props.idText}
+                value={props.varText}
+                placeholder={props.placeholderText}
+                className="w-full border rounded px-3 py-2 resize-none overflow-hidden focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+        </div>
+
+    )
+}
