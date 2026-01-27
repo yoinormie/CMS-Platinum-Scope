26508585b3c617aec465f0a5d7e3285d71288305
yoelgarcialago <yoelgarcialago@gmail.com>
2026-01-27 16:53:38 +0100

    Ventana definida globalmente, preload añadido, node integrado e ipcMain para abrir ficheros y directorios

--- electron/main.ts
+++ electron/main.ts
@@ -1,17 +1,20 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, ipcMain, dialog } from 'electron';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import path from 'path';
+
+let win: BrowserWindow;
 
 function createWindow() {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = dirname(__filename);
 
-    const win = new BrowserWindow({
+    win = new BrowserWindow({
         width: 1200,
         height: 800,
         webPreferences: {
-            nodeIntegration: false,
+            preload: path.join(__dirname, './preload.ts'),
+            nodeIntegration: true,
             contextIsolation: true
         }
     });
@@ -36,3 +39,26 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit();
 });
+
+ipcMain.handle('open-file', async () => {
+    if (!win) return null;
+
+    const result = await dialog.showOpenDialog(win, {
+        properties: ['openFile']
+    });
+
+    if (result.canceled) return null;
+    return result.filePaths[0];
+});
+
+ipcMain.handle('open-directory', async () => {
+
+    const result = await dialog.showOpenDialog(win, {
+        properties: ['openDirectory']
+    });
+
+    if (result.canceled) return null;
+    return result.filePaths[0];
+});
+
+
