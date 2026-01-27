6aab4d14c77f5ca9858f7bf63e3f29b6474322b0
yoelgarcialago <yoelgarcialago@gmail.com>
2026-01-27 17:33:27 +0100

    IpcHandlers de electron-store definidos para api y definidos en global

--- electron/preload.ts
+++ electron/preload.ts
@@ -2,5 +2,7 @@ import { contextBridge, ipcRenderer } from 'electron';
 
 contextBridge.exposeInMainWorld('api', {
   openFile: () => ipcRenderer.invoke('open-file'),
-  openDirectory: () => ipcRenderer.invoke('open-directory')
+  openDirectory: () => ipcRenderer.invoke('open-directory'),
+  setStorePath: (key: string, value: string) => ipcRenderer.invoke('set-path', key, value),
+  getStorePath: (key: string) => ipcRenderer.invoke('get-path', key)
 });
--- react-source/src/global.d.ts
+++ react-source/src/global.d.ts
@@ -1,10 +1,12 @@
-export {};
+export { };
 
 declare global {
-  interface Window {
-    api: {
-      openFile: () => string
-      openDirectory: () => string
-    };
-  }
+    interface Window {
+        api: {
+            openFile: () => string
+            openDirectory: () => string
+            setStorePath: (key: keyof StoreSchema, value: string) => Promise<void>;
+            getStorePath: (key: keyof StoreSchema) => Promise<string | null>;
+        };
+    }
 }
