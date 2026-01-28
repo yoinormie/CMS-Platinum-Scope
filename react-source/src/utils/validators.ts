92a68f563c698b8305bcdb5f78cc09b38658de47
yoelgarcialago <yoelgarcialago@gmail.com>
2026-01-28 11:36:55 +0100

    Validador para saber si se va a guardar lo necesario

---
+++ react-source/src/utils/validators.ts
@@ -0,0 +1,19 @@
+import { useRequirementsForm } from "../constants/requisitosConstants";
+import { useReviewForm } from "../constants/opinionConstants";
+import type { EnlaceCompra } from "../types/enlaceCompraType";
+
+export function canSaveMainForm(data: {
+  titulo: string;
+  selectedImage: File | null;
+  minRequirements: ReturnType<typeof useRequirementsForm>;
+  opinion: ReturnType<typeof useReviewForm>;
+  enlacesCompra: EnlaceCompra[]
+}): boolean {
+  return (
+    !!data.titulo.trim() &&
+    !!data.selectedImage &&
+    data.minRequirements.isValid() &&
+    data.opinion.isValid() &&
+    !(data.enlacesCompra.length === 0)
+  );
+}
