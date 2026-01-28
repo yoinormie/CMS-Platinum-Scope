97f58a05035c1f29d785532c33d555fb1a4db4ff
yoelgarcialago <yoelgarcialago@gmail.com>
2026-01-28 19:48:58 +0100

    Cambio de tipo para la imagen (pasamos de archivo a ruta)

--- react-source/src/utils/validators.ts
+++ react-source/src/utils/validators.ts
@@ -4,20 +4,20 @@ import type { EnlaceCompra } from "../types/enlaceCompraType";
 
 export function canSaveMainForm(data: {
   titulo: string;
-  selectedImage: File | null;
+  selectedImage: { name: string; path: string } | null;
   minRequirements: ReturnType<typeof useRequirementsForm>;
   opinion: ReturnType<typeof useReviewForm>;
-  enlacesCompra: EnlaceCompra[]
-  desarrollador: string
-  editor: string
-  plataformas: string
+  enlacesCompra: EnlaceCompra[];
+  desarrollador: string;
+  editor: string;
+  plataformas: string;
 }): boolean {
   return (
     !!data.titulo.trim() &&
-    !!data.selectedImage &&
+    !!data.selectedImage?.path &&
     data.minRequirements.isValid() &&
     data.opinion.isValid() &&
-    !(data.enlacesCompra.length === 0) &&
+    data.enlacesCompra.length > 0 &&
     !!data.desarrollador.trim() &&
     !!data.editor.trim() &&
     !!data.plataformas.trim()
