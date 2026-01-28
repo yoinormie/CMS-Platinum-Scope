65162c32e23342477fb5b31b738b7ed4c16998d2
yoelgarcialago <yoelgarcialago@gmail.com>
2026-01-28 15:12:06 +0100

    Estado de los campos extraido

--- react-source/src/components/recRequirementsSets.tsx
+++ react-source/src/components/recRequirementsSets.tsx
@@ -1,57 +1,60 @@
 import TextSet from "./TextSet"
 import { useRequirementsForm } from "../constants/requisitosConstants"
 
-export function RecRequirementsSets() {
-    const recRequirements = useRequirementsForm();
+type Props = {
+    form: ReturnType<typeof useRequirementsForm>;
+};
+
+export function RecRequirementsSets({ form }: Props) {
     return (
         <>
             <TextSet
-                idText='rec_so'
-                labelText='SO'
-                varText={recRequirements.so}
-                setVarText={recRequirements.setSO}
+                idText="rec_so"
+                labelText="SO"
+                varText={form.so}
+                setVarText={form.setSO}
             />
             <TextSet
-                idText='rec_cpu'
-                labelText='CPU'
-                varText={recRequirements.cpu}
-                setVarText={recRequirements.setCpu}
+                idText="rec_cpu"
+                labelText="CPU"
+                varText={form.cpu}
+                setVarText={form.setCpu}
             />
             <TextSet
-                idText='rec_ram'
-                labelText='RAM'
-                varText={recRequirements.ram}
-                setVarText={recRequirements.setRam}
+                idText="rec_ram"
+                labelText="RAM"
+                varText={form.ram}
+                setVarText={form.setRam}
             />
             <TextSet
-                idText='rec_gpu'
-                labelText='GPU'
-                varText={recRequirements.gpu}
-                setVarText={recRequirements.setGpu}
+                idText="rec_gpu"
+                labelText="GPU"
+                varText={form.gpu}
+                setVarText={form.setGpu}
             />
             <TextSet
-                idText='rec_directx'
-                labelText='DirectX'
-                varText={recRequirements.directX}
-                setVarText={recRequirements.setDirectx}
+                idText="rec_directx"
+                labelText="DirectX"
+                varText={form.directX}
+                setVarText={form.setDirectx}
             />
             <TextSet
-                idText='rec_red'
-                labelText='Red'
-                varText={recRequirements.red}
-                setVarText={recRequirements.setRed}
+                idText="rec_red"
+                labelText="Red"
+                varText={form.red}
+                setVarText={form.setRed}
             />
             <TextSet
-                idText='rec_disco'
-                labelText='Disco'
-                varText={recRequirements.disco}
-                setVarText={recRequirements.setDisco}
+                idText="rec_disco"
+                labelText="Disco"
+                varText={form.disco}
+                setVarText={form.setDisco}
             />
             <TextSet
-                idText='rec_notas'
-                labelText='Notas'
-                varText={recRequirements.notas}
-                setVarText={recRequirements.setNotas}
+                idText="rec_notas"
+                labelText="Notas"
+                varText={form.notas}
+                setVarText={form.setNotas}
             />
         </>
     )
